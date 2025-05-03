d71fb160ca322fb7f121e1c69bf6a10e53d069ca
Jenny Veens <jennyveens@gmail.com>
2025-05-02 21:21:08 -0400

    TST-16: More base styles

--- header.php
+++ header.php
@@ -33,12 +33,4 @@ namespace ToppStart;
       <?php get_template_part( 'template-parts/header/navigation' ); ?>
     </header>
 
-    <div class="container">
-      <div class="row">
-        <div class="col-4">THing1</div>
-        <div class="col-4">Thing2</div>
-        <div class="col-4">THing3</div>
-      </div>
-    </div>
-
     <div class="site-content">
